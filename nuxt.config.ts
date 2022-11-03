e2157177138c09e1ac1f2acf1e683307f6d3186e
Tharin <Tharin.m.wilk@gmail.com>
2022-11-03 17:48:51 -0400

    Removed SSR and added app baseurl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: false,
   target: "static",
   router: {
     base: "/nuxt-test/",
+  },
+  app: {
+    baseURL: "http://tharinwilk.github.io/nuxt-test/",
   },
   modules: ["nuxt-icon"],
 });
