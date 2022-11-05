1a2409418782d002fcbb66499c599ab0ace70e65
Tharin <Tharin.m.wilk@gmail.com>
2022-11-05 08:15:49 -0400

    Reapplied define nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default {
+export default defineNuxtConfig({
   target: "static",
 
   router: {
@@ -15,4 +15,4 @@ export default {
   },
 
   modules: ["nuxt-icon"],
-};
+});
