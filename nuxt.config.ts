644b5211b0b66b3081f90e932e4c2738a3c5427d
Tharin <Tharin.m.wilk@gmail.com>
2022-11-05 07:59:26 -0400

    testing removed definenuxtconfig function

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
+export default {
   target: "static",
+
+  router: {
+    base: "/nuxt-test/",
+  },
+
   modules: ["nuxt-icon"],
-});
+};
